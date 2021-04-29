2d7dfe06df8ce00e9c1a8e6cc3e3de67c58eae0f
mokuo <tennis10988.yk@gmail.com>
2021-04-29 17:47:12 +0900

    ch02/p11

--- ch02/p11.py
+++ ch02/p11.py
@@ -7,4 +7,5 @@ with open("./popular-names.txt") as f:
         results.append(re.sub("\t", " ", line))
 
 with open("./output.txt", mode="w") as f:
+    # sed s/$'\t'/$' '/g popular-names.txt > ./output2.txt
     f.writelines(results)
